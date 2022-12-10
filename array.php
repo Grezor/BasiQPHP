837eba119bfa1291637724d7024140e6c65adf42
Geoffrey Duplessi <grezor.gamer@gmail.com>
2022-12-10 17:42:16 +0100

    start to lesson array

---
+++ array.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Les tableaux
+ * 
+ */
+
+// $notes = [10, 20, 9, 8];
+// echo $notes[1]; // 20
+// echo $notes[3]; // 8
+
+$eleves = ['marc', 'doe', [10, 4, 2]];
+
+
---
+++ variable.php
@@ -0,0 +1,13 @@
+<?php 
+// les variables 
+$nom = 'Doe';
+$prenom = 'Geoffrey';
+// echo $nom;
+// echo $prenom . $nom;
+
+$note = 10;
+$note2 = 15;
+echo ($note + $note2) / 2;
+
+echo 'Bonjour' . $prenom . '' . $nom . 'vous avez eu ' . (($note + $note2) / 2) . ' de moyenne';
+?>
