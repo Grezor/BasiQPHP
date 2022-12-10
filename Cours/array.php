b1838da357f6be40a0e4dfced2e1d2213e590dc5
Geoffrey Duplessi <grezor.gamer@gmail.com>
2022-12-10 18:34:43 +0100

    save array

--- Cours/array.php
+++ Cours/array.php
@@ -8,6 +8,30 @@
 // echo $notes[1]; // 20
 // echo $notes[3]; // 8
 
-$eleves = ['marc', 'doe', [10, 4, 2]];
+$eleve = ['marc', 'doe', [10, 4, 2]];
+// echo $eleve[2][1];
 
+$arrayV2 = [
+    'nom' => 'doe',
+    'prenom' => 'Marc',
+    'notes' => [10, 4, 2]
+];
+// $arrayV2['prenom'] = 'paul'; // pour changer la valeur
+$arrayV2['notes'][3] = 20; // add new note
+// echo $arrayV2['prenom'] . ' ' . $arrayV2['nom'];
+// print_r($arrayV2['notes']);
 
+$classe = [
+    [
+        'nom' => 'doe',
+        'prenom' => 'Marc',
+        'notes' => [10, 4, 2]
+    ], 
+    [
+        'nom' => 'doe',
+        'prenom' => 'Jeanne',
+        'notes' => [2, 4, 2]
+    ]
+];
+
+// echo $classe[1]['notes'][1];
